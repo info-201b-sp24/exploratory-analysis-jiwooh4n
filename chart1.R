503cc663c3fe27cc8e73a969bf5fed821c313923
szzzara <szzzara@uw.edu>
2024-05-15 12:18:56 -0700

    sara

--- chart1.R
+++ chart1.R
@@ -1,1 +1,19 @@
 # hello 
+library(dplyr)
+
+# loading my data file 
+
+dataset1 <- read.csv("dataset1.csv")
+
+# cleaning my data
+
+dataset1 <- na.omit(dataset1)
+
+# selecting only necessary info
+
+dataset1 <- select(dataset1, Age, Sleep.Duration, Quality.of.Sleep, Stress.Level)
+
+# merging this dataset with dataset2 
+
+#  range 
+
